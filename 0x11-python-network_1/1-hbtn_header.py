157bf4e52240a437f38fdbeeaf55f09de16a36ac
P-Ade <109440726+P-Ade@users.noreply.github.com>
2023-04-01 06:07:22 +0100

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,8 @@
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header ofthe response.
 """
+
+
 import urllib.request, sys
 
 if __name__ == '__main__':
